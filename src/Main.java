30c57f7cff245e40f6ef6d06e15851842a11b32f
kkufaas <kristina.kufaas@gmail.com>
2023-04-09 12:49:27 +0200

    created first caclulator version

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        System.out.println(a+b);
+
+        //red color - not versioned files (не отслеживаемые)
+        //green color - versioned files (отслеживаемые)
+
+    }
+}
