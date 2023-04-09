2d7ece370d5bd76d5b651b7120ef13050815d0f4
kkufaas <kristina.kufaas@gmail.com>
2023-04-09 12:54:33 +0200

    added console input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 5;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scn.nextInt();
+        System.out.print("Enter b: ");
+        int b = scn.nextInt();
         System.out.println(a+b);
 
         //red color - not versioned files (не отслеживаемые)
